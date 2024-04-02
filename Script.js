b779cd6804c9a0eadb84d40d58390e5ff5e3980e
notBhavik <154809700+notBhavik@users.noreply.github.com>
2024-04-02 17:56:21 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,27 +1,79 @@
-async function fetchWheatherCity(cityName) {
-    let url=https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a6821afe4630c8b189f6a64e5950bcb
-    const response=await fetch(url);
-    const data=await response.json();
-    let city=document.getElementById("city");
-    city.innerHTML=data.name;
-    let windSpeed= document.getElementById("wind_speed");
-    windSpeed.innerHTML=data.wind.speed +"km/hr";
-    let humidity= document.getElementById("humidity");
-    humidity.innerHTML=data.main.humidity+"g/kg";
-    let temp=document.getElementById("temp"); 
-    temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C"
-    console.log(data)
+* { 
+padding: 0px;
+margin: 0px;
+box-sizing: border-box;
 }
-//demo()
 
-function fetchCity(){
-    let cityName= document.getElementById("city_name");
-    if(cityName.value==="")
-    {
-        alert("Enter a city name");
-    }
-    else{
-        fetchWheatherCity(cityName.value);
-        cityName.value="";
-    }
+.mdiv{
+height: 100vh;
+width: 100%;
+position: fixed;
+top: 0px;
+Left: 0px;
+display: flex;
+align-items: center;
+justify-content: center;
+background-image: url("https://external-preview.redd.it/MlJ8XV17UcNiF3npHX4dJKYUSYnNE6mEmFL91X_rBZU.jpg?auto=webp&s=c2bc8bdf5a455eed4bdad1d4743fa2360dd0e743");
+background-repeat: no-repeat;
+background-position: center;
+background-size: cover;
+}
+
+.container{
+height: 350px;
+width: 250px;
+box-shadow: 0px 0px 20px grey;
+border-radius: 30px;
+padding: 20px;
+background: transparent;
+backdrop-filter: blur(20px);
+
+.heading{
+color: royalblue;
+font-family: Arial Black;
+font-size: 15px;
+text-align: center;
+margin-bottom: 15px;
+}
+
+input[type="text"]{
+height: 30px;
+width: 180px;
+padding-left: 10px;
+outline: none;
+font-weight: bolder;
+text-transform: uppercase;
+}
+
+.srch_btn{
+border: none;
+background-color: transparent; padding-top: 20px;
+cursor: pointer;
+}
+
+.component{
+display: flex;
+align-items: center;
+justify-content: center;
+}
+
+.btn{
+ margin-top: -20px;
+margin-left: 10px;
+}
+
+ #city{
+ color: orangered;
+font-family: Arial Black;
+font-size: 20px;
+
+.measurement{
+font-family: sans-serif;
+font-weight: bolder;
+
+
+.headings{
+color: royalblue;
+font-family: Arial Black;
+font-size: 12px; 
 }
