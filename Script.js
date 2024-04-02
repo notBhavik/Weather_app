1acac19f45cc7ff74e4efa11b630b6f9b8a7c986
notBhavik <154809700+notBhavik@users.noreply.github.com>
2024-04-02 17:18:15 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,21 +1,27 @@
 async function fetchWeatherData(cityName){
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=466c38de8751406baded98a9f451b33e `;
-    const response = await fetch(url)
-    const data = await response.json();
-    console.log(data)
-
+	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=24c1524d8b37eda85bebe85c3b5b8459`;
+	const response = await fetch(url);
+	const data = await response.json();
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML =data.wind.speed+" km/hr"; 
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+" %";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
+	console.log(data);
 }
 
-// demo()
+
 
 function fetchCity(){
-    let cityName= document.getElementById("city_name") 
-    if(cityName.value===""){
-        alert("Enter a city name");
-    }
-    else{
-        fetchWeatherData(cityName.value);
-        cityName.value=""
-    }
-
+	let cityName = document.getElementById("city_name");
+	if (cityName.value==="") {
+		alert("Enter a city name");
+	}
+	else{
+		fetchWeatherData(cityName.value);
+		cityName.value="";
+	}
 }
