d6ba91bef1c7ea2a4e3bc3df18d31eb64281bcb4
notBhavik <154809700+notBhavik@users.noreply.github.com>
2024-04-02 17:37:58 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,27 +1,27 @@
-async function fetchWeatherData(cityName){
-	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=24c1524d8b37eda85bebe85c3b5b8459`;
-	const response = await fetch(url);
-	const data = await response.json();
-	let city = document.getElementById("city");
-	city.innerHTML = data.name;
-	let windSpeed = document.getElementById("wind_speed");
-	windSpeed.innerHTML =data.wind.speed+" km/hr"; 
-	let humidity = document.getElementById("humidity");
-	humidity.innerHTML = data.main.humidity+" %";
-	let temp = document.getElementById("temp");
-	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
-	console.log(data);
+async function fetchWheatherCity(cityName) {
+    let url=https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7a6821afe4630c8b189f6a64e5950bcb
+    const response=await fetch(url);
+    const data=await response.json();
+    let city=document.getElementById("city");
+    city.innerHTML=data.name;
+    let windSpeed= document.getElementById("wind_speed");
+    windSpeed.innerHTML=data.wind.speed +"km/hr";
+    let humidity= document.getElementById("humidity");
+    humidity.innerHTML=data.main.humidity+"g/kg";
+    let temp=document.getElementById("temp"); 
+    temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C"
+    console.log(data)
 }
-
-
+//demo()
 
 function fetchCity(){
-	let cityName = document.getElementById("city_name");
-	if (cityName.value==="") {
-		alert("Enter a city name");
-	}
-	else{
-		fetchWeatherData(cityName.value);
-		cityName.value="";
-	}
+    let cityName= document.getElementById("city_name");
+    if(cityName.value==="")
+    {
+        alert("Enter a city name");
+    }
+    else{
+        fetchWheatherCity(cityName.value);
+        cityName.value="";
+    }
 }
